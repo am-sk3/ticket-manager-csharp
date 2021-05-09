8b10de3755222ae933eab76dc7900d8e10ec236a
André Mateus <extreme.sk3@gmail.com>
2021-05-09 01:59:21 +0100

    implemented ticket service

---
+++ TicketManager/Services/ITicketService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TicketManager.Repository.Models;
+using TicketManager.ViewModels.Ticket;
+
+namespace TicketManager.Services
+{
+    public interface ITicketService
+    {
+        Task<int> ChangeStatusAsync(TicketStatus status, int ticketID);
+        Task<int> CreateAsync(TicketAddViewModel ticket);
+        Task<IEnumerable<TicketGetAllViewModel>> GetAllAsync();
+        Task<IEnumerable<TicketGetAllViewModel>> GetByCompany(int companyID);
+        Task<TicketGetViewModel> GetByIdAsync(int id);
+        Task<IEnumerable<TicketGetAllViewModel>> GetByUser(int userID);
+        Task<int> RemoveAsync(int id);
+    }
+}
--- TicketManager/Services/TicketService.cs
+++ TicketManager/Services/TicketService.cs
@@ -4,52 +4,59 @@ using System.Linq;
 using System.Threading.Tasks;
 using TicketManager.Repository;
 using TicketManager.ViewModels.Ticket;
+using TicketManager.Extensions.ViewModel;
+using TicketManager.Repository.Models;
 
 namespace TicketManager.Services
 {
-    public class TicketService
+    public class TicketService : ITicketService
     {
-        private readonly ITicketRepository _ticketRepository;
+        private readonly ITicketRepository _ticket;
 
         public TicketService(ITicketRepository ticketRepository)
         {
-            _ticketRepository = ticketRepository;
+            _ticket = ticketRepository;
         }
 
         public async Task<IEnumerable<TicketGetAllViewModel>> GetAllAsync()
         {
-            //var result = await _user.GetAll();
-            //return result.Select(x => x.ToViewModel());
-            throw new NotImplementedException();
+            var result = await _ticket.GetAll();
+            return result.Select(x => x.ToGetAllViewModel());
         }
 
         public async Task<TicketGetViewModel> GetByIdAsync(int id)
         {
-            //var result = await _user.GetByID(userID);
-            //return result.ToViewModel();
-            throw new NotImplementedException();
+            var result = await _ticket.GetById(id);
+            return result.ToViewModel();
+        }
+
+        public async Task<IEnumerable<TicketGetAllViewModel>> GetByCompany(int companyID)
+        {
+            var result = await _ticket.GetByCompany(companyID);
+            return result.Select(x => x.ToGetAllViewModel());
+        }
+
+        public async Task<IEnumerable<TicketGetAllViewModel>> GetByUser(int userID)
+        {
+            var result = await _ticket.GetByUser(userID);
+            return result.Select(x => x.ToGetAllViewModel());
         }
 
         public async Task<int> CreateAsync(TicketAddViewModel ticket)
         {
-            //var request = user.ToUserModel();
+            var request = ticket.ToTicketModel();
 
-            //return await _user.Create(request);
-            throw new NotImplementedException();
+            return await _ticket.Create(request);
         }
 
-        //public async Task<int> EditAsync(UserAddEditViewModel user, int userID)
-        //{
-        //    var request = user.ToUserModel();
-        //    request.ID = userID;
-
-        //    return await _user.Update(request);
-        //}
+        public async Task<int> ChangeStatusAsync(TicketStatus status, int ticketID)
+        {
+            return await _ticket.ChangeStatus(status, ticketID);
+        }
 
         public async Task<int> RemoveAsync(int id)
         {
-            //return await _user.Delete(userID);
-            throw new NotImplementedException();
+            return await _ticket.Delete(id);
         }
 
     }
