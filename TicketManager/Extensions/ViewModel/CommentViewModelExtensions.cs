c55271ef48b4b2f95e954cb09482bea60e67572e
André Mateus <extreme.sk3@gmail.com>
2021-05-09 01:47:39 +0100

    implemented model extensions

---
+++ TicketManager/Extensions/ViewModel/CommentViewModelExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketManager.Repository.Models;
+using TicketManager.ViewModels.Comment;
+
+namespace TicketManager.Extensions.ViewModel
+{
+    public static class CommentViewModelExtensions
+    {
+        public static CommentGetViewModel ToViewModel(this Comment comment)
+        {
+            return new CommentGetViewModel
+            {
+                Content = comment.Content,
+                CreationDate = comment.CreationDate,
+                ID = comment.ID,
+                UserID = comment.UserID
+            };
+        }
+
+        public static Comment ToModel(this CommentAddEditViewModel comment)
+        {
+            return new Comment
+            {
+                Content = comment.Content,
+                CreationDate = comment.CreationDate,
+                ID = comment.ID,
+                TicketID = comment.TicketID,
+                UserID = comment.UserID
+            };
+        }
+    }
+}
---
+++ TicketManager/Extensions/ViewModel/TicketViewModelExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketManager.Repository.Models;
+using TicketManager.ViewModels.Ticket;
+
+namespace TicketManager.Extensions.ViewModel
+{
+    public static class TicketViewModelExtensions
+    {
+        public static TicketGetViewModel ToViewModel(this Ticket ticket)
+        {
+            return new TicketGetViewModel
+            {
+                ID = ticket.ID,
+                Status = ticket.Status,
+                Subject = ticket.Subject,
+                CloseDate = ticket.CloseDate,
+                CompanyID = ticket.CompanyID,
+                Content = ticket.Content,
+                CreationDate = ticket.CreationDate,
+                LastUpdateDate = ticket.LastUpdateDate,
+                LastUpdateUserID = ticket.LastUpdateUserID,
+                UserID = ticket.UserID,
+            };
+        }
+
+        public static TicketGetAllViewModel ToGetAllViewModel(this Ticket ticket)
+        {
+            return new TicketGetAllViewModel
+            {
+                ID = ticket.ID,
+                UserID = ticket.UserID,
+                CompanyID = ticket.CompanyID,
+                CreationDate = ticket.CreationDate,
+                Status = ticket.Status,
+                Subject = ticket.Subject
+            };
+        }
+
+        public static Ticket ToModel(this TicketAddViewModel ticket)
+        {
+            return new Ticket
+            {
+                Subject = ticket.Subject,
+                Content = ticket.Content,
+                CompanyID = ticket.CompanyID,
+                UserID = ticket.UserID
+            };
+        }
+    }
+}
